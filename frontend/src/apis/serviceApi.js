3126fd4e8d6db5241971c753e999db49592714d6
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2025-09-10 20:11:51 +0600

    frontend: fixed ManageNewsletter and ManageServices issues and added serviceApi

---
+++ frontend/src/apis/serviceApi.js
@@ -0,0 +1,73 @@
+import api from "./api"; // adjust path
+
+export default class ServiceApi {
+  constructor(baseURL = "http://localhost:5000") {
+    this.serviceApi = api; // reuse existing axios instance
+    this.baseURL = baseURL+"/service";
+  }
+
+  async createService(
+    {title,
+    short_desc,
+    key_benefits,
+    our_process,
+    active,
+    icon}
+    ) {
+    try {
+      const response = await this.serviceApi.post(`${this.baseURL}/`, 
+        {
+            title,
+            short_desc,
+            key_benefits,
+            our_process,
+            active,
+            icon
+        });
+      return response.data;
+    } catch (err) {
+      throw err.response?.data || { error: "Failed to create service" };
+    }
+  }
+
+  async getServices() {
+    try {
+      const response = await this.serviceApi.get(`${this.baseURL}/`);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      throw error.response?.data || { error: "Failed to fetch services" };
+    }
+  }
+
+
+  
+  async updateService(service_id,
+    title,
+    short_desc,
+    key_benefits,
+    our_process,
+    active,
+    icon) {
+    try {
+      const response = await this.serviceApi.put(`${this.baseURL}/${service_id}`, 
+        { 
+            title,
+            short_desc,
+            key_benefits,
+            our_process,
+            active,
+            icon 
+        });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      throw error.response?.data || { error: "Failed to update service" };
+    }
+  }
+
+  
+
+
+
+}
