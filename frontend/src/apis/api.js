14482d73174f6734420d55ba20b69611f2657474
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2025-10-03 20:38:16 +0600

    setup

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useUserStore from "../stores/userStore";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.BACKEND_URL,
   withCredentials: true, // 👈 send cookies
 });
 
